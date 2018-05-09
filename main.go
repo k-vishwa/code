c71a3900109a9a925ee070cfcf8a4fdcb519569e
Vishwanath K <vishwanath.k@kloud9.nyc>
2018-05-09 14:56:33 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ var HelloMessage string
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 // 	out, _ := exec.Command("bash", "-c", "hostname").Output()
 // 	HelloMessage = "#############" + string(out)
-	HelloMessage = "Hello World-7"
+	HelloMessage = "Hello World-71"
 	fmt.Println(HelloMessage)
   	fmt.Fprintf(w, HelloMessage)
 }
